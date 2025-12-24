02c16ebd2909b001f9308c1a2a60a67d4b84444e
fax-spittah <eddytiger7@gmail.com>
2025-12-24 23:31:42 +0100

    feat: add DisplayBudgetStatusComponent to home page

--- app/pages/HomePage.tsx
+++ app/pages/HomePage.tsx
@@ -1,10 +1,15 @@
+import DisplayBudgentStatusComponent from "../components/DisplayBudgentStatusComponent";
 import SetBudgetComponent from "../components/SetBudgetComponent";
 
 export default function HomePage() {
   return (
     <div>
       <h1>Shopping List</h1>
+      <hr />
       <SetBudgetComponent />
+      <hr />
+      <DisplayBudgentStatusComponent />
+      <hr />
     </div>
   );
 }
