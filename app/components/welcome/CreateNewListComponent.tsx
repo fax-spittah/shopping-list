e2552890ae87e4c29c18f0c1ec55dcd6db69cfab
fax-spittah <eddytiger7@gmail.com>
2025-12-25 20:02:50 +0100

    fix: styling new list component

--- app/components/welcome/CreateNewListComponent.tsx
+++ app/components/welcome/CreateNewListComponent.tsx
@@ -4,22 +4,26 @@ import { Label } from "@/components/ui/label";
 
 export default function CreateNewListComponent() {
   return (
-    <>
-      <div>
-        <h2>Start a New Shopping List</h2>
-        <Label htmlFor="listName">List Name (optional):</Label>
-        <Input type="text" id="listName" />
-        <br />
-        <Label htmlFor="initialBudget">Budget (required):</Label>
-        <Input
-          type="number"
-          id="initialBudget"
-          placeholder="Initial Budget (optional)"
-        />
-        <br />
-        <Button variant="outline">Start budgeting</Button>
+    <div className="bg-white rounded-xl p-6 mt-6 shadow-md max-w-md w-full mx-auto">
+      <h2 className="text-2xl font-bold mb-4">Start a New Shopping List</h2>
+      <div className="flex flex-col gap-4">
+        <div>
+          <Label htmlFor="listName">List Name (optional):</Label>
+          <Input type="text" id="listName" className="mt-1 w-full" />
+        </div>
+        <div>
+          <Label htmlFor="initialBudget">Budget (required):</Label>
+          <Input
+            type="number"
+            id="initialBudget"
+            placeholder="Initial Budget"
+            className="mt-1 w-full"
+          />
+        </div>
+        <Button variant="outline" className="mt-2 w-full">
+          Start budgeting
+        </Button>
       </div>
-      <hr />
-    </>
+    </div>
   );
 }
