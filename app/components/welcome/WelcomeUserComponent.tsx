6324522a6a011a0703152dab6678b552b17931f9
fax-spittah <eddytiger7@gmail.com>
2025-12-25 20:04:45 +0100

    fix: styling welcome user component

--- app/components/welcome/WelcomeUserComponent.tsx
+++ app/components/welcome/WelcomeUserComponent.tsx
@@ -1,16 +1,13 @@
 export default function WelcomeUserComponent() {
   return (
-    <>
-      <div>
-        <h2>
-          Welcome to <b>BudgetBuddy!</b>
-        </h2>
-        <p>
-          Track your shopping, spend with sense, save some cents and avoid the
-          stress!
-        </p>
-      </div>
-      <hr />
-    </>
+    <div className="bg-blue-50 rounded-xl p-6 mt-6 shadow-md text-center">
+      <h2 className="text-3xl font-bold mb-2">
+        Welcome to <span className="text-blue-600">BudgetBuddy!</span>
+      </h2>
+      <p className="text-gray-700">
+        Track your shopping, spend with sense, save some cents, and avoid the
+        stress!
+      </p>
+    </div>
   );
 }
