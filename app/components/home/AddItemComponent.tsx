d068cf18f2a810100c928436f158efda6c658d0c
fax-spittah <eddytiger7@gmail.com>
2026-01-09 17:41:37 +0100

    feat: adding form data validation logic to the form

--- app/components/home/AddItemComponent.tsx
+++ app/components/home/AddItemComponent.tsx
@@ -1,8 +1,37 @@
+"use client";
 import { Minimize2 } from "lucide-react";
+import { useState } from "react";
 
 export default function AddItemComponent() {
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [showErrorMessage, setShowErrorMessage] = useState<boolean>(false);
+
+  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
+    const formData = new FormData(event.currentTarget);
+    checkFormFields(formData);
+    event.preventDefault();
+  }
+
+  function checkFormFields(formData: FormData) {
+    const itemName = formData.get("itemName");
+    const itemPrice = formData.get("itemPrice");
+    const itemQuantity = formData.get("itemQuantity");
+
+    if (!itemName || !itemPrice || !itemQuantity) {
+      setErrorMessage("Please fill in all fields");
+      setShowErrorMessage(true);
+    } else {
+      setErrorMessage("");
+      setShowErrorMessage(false);
+    }
+  }
+
   return (
-    <div className="bg-white border-rounded w-[95%] mx-auto p-4 flex flex-col gap-3 shadow-md">
+    <form
+      className="bg-white border-rounded w-[95%] mx-auto p-4 flex flex-col gap-3 shadow-md"
+      onSubmit={onSubmit}
+      action={checkFormFields}
+    >
       <div className="flex justify-between items-center mb-2">
         <div className="flex-1 text-center">
           <h2 className="font-bold text-lg">Add New Item</h2>
@@ -15,6 +44,7 @@ export default function AddItemComponent() {
         </label>
         <input
           id="itemName"
+          name="itemName"
           type="text"
           placeholder="Item name"
           className="flex-2"
@@ -26,6 +56,7 @@ export default function AddItemComponent() {
         </label>
         <input
           id="itemPrice"
+          name="itemPrice"
           type="number"
           placeholder="Price"
           className="flex-2"
@@ -37,14 +68,24 @@ export default function AddItemComponent() {
         </label>
         <input
           id="itemQuantity"
+          name="itemQuantity"
           type="number"
           placeholder="Quantity"
           className="flex-2"
         />
       </div>
-      <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+      <p
+        className={`${showErrorMessage ? "text-red-500" : "hidden"}`}
+        id="errorMessage"
+      >
+        {errorMessage}
+      </p>
+      <button
+        type="submit"
+        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+      >
         Add Item
       </button>
-    </div>
+    </form>
   );
 }
