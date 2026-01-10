2eadd6bd83231f68378629508f1c6056ec3e8920
fax-spittah <eddytiger7@gmail.com>
2026-01-10 16:41:41 +0100

    fix: cleaning up unused components from home page

--- app/components/home/SetBudgetComponent.tsx
+++
@@ -1,9 +0,0 @@
-export default function SetBudgetComponent() {
-  return (
-    <div>
-      <h2>Set Your Budget</h2>
-      <input type="number" placeholder="Enter your budget" />
-      <button>Set Budget</button>
-    </div>
-  );
-}
