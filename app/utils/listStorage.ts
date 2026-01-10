1f26ed08f40e86d35fbf50bb3a8bd1ccbf3af7bb
fax-spittah <eddytiger7@gmail.com>
2026-01-10 16:29:53 +0100

    feat: writing the creatList function for the creation of a new list along with unique id generation and item storage to Localstorage

---
+++ app/utils/listStorage.ts
@@ -0,0 +1,20 @@
+import { ShoppingListProps } from "../types/shopping";
+
+function createList(initialBudget: number, listName?: string) {
+  const newList: ShoppingListProps = {
+    id: generateUniqueId(),
+    listName: listName ? listName : "My Shopping List",
+    budget: initialBudget,
+    items: [],
+  };
+  saveListToStorage(newList);
+  return newList;
+}
+
+function generateUniqueId(): string {
+  return "_" + Math.random().toString(36);
+}
+
+function saveListToStorage(list: ShoppingListProps) {
+  localStorage.setItem("shoppingLists", JSON.stringify([list]));
+}
