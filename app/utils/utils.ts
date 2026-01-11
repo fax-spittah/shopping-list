39225af4f8c6abc448c8399c4399495d05f32fb7
fax-spittah <eddytiger7@gmail.com>
2026-01-11 19:53:12 +0100

    fix: navigating to the welcome page from the home if no list was created to avoid issues

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -1,3 +1,10 @@
+import { useRouter } from "next/navigation";
+
 export function generateUniqueId(): string {
   return "_" + Math.random().toString(36);
+}
+
+export function useNavigateToWelcomePage() {
+  const router = useRouter();
+  return () => router.push("/");
 }
